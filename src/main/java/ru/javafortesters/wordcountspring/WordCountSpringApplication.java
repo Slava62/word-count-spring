662a9eca4764c80f315186026dd5ecaa58099df7
Frolov Vacheslav <fvg.personalbox@gmail.com>
2020-09-02 23:18:46 +0300

    Initial commit

---
+++ src/main/java/ru/javafortesters/wordcountspring/WordCountSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.javafortesters.wordcountspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordCountSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordCountSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/javafortesters/wordcountspring/WordCountSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javafortesters.wordcountspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordCountSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
